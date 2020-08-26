a0be2c3f232ad181db0fed838cb040558b328c70
werliton <werliton.carlos@ceuma.br>
2020-08-25 22:40:47 -0300

    Cadastrando agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,13 +1,36 @@
 import {
-  Router, Request, Response,
+  Router,
 } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.post('/', (request: Request, response: Response) => response.json({
-  message: 'Ola mundo',
-}));
+interface Appointment{
+    id: string
+    provider: string
+    data: string
+}
 
-appointmentsRouter.get('/', (request, response) => response.json({ message: 'oi' }));
+type Appointments = Array<Appointment>
+
+const appointments: Appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, data } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    data,
+  };
+
+  appointments.push(appointment);
+
+  return response.status(201).json({
+    message: 'Cadastrado com sucesso',
+  });
+});
+
+appointmentsRouter.get('/', (request, response) => response.json(appointments));
 
 export default appointmentsRouter;
