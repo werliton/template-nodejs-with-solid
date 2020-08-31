c23176be18b9c670aa91a9ae49ba51241e317f87
werliton <werliton.carlos@ceuma.br>
2020-08-31 11:45:34 -0300

    Validando credenciais

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import AuthenticationUserService from '../services/AuthenticationUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticationUser = new AuthenticationUserService();
+
+    const { user } = await authenticationUser.execute({
+      email, password,
+    });
+
+    delete user.password;
+
+    return response.status(201).json({ user });
+  } catch (err) {
+    return response.status(400).json({
+      error: err.message,
+    });
+  }
+});
+
+export default sessionsRouter;
