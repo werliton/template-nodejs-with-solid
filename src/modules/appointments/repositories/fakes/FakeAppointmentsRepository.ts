7a2b1e5a503921ecf6fd5ff96be00077f6899a70
werliton <werliton.carlos@ceuma.br>
2020-09-16 15:28:11 -0300

    Criando o primeiro teste

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import { uuid } from 'uuidv4';
+import IAppointmentsRepository from '../IAppointmentsRepository';
+
+// Usando o L do SOLID
+class FakeAppointmentsRepository implements IAppointmentsRepository {
+  findAll(): Promise<Appointment[]> {
+    throw new Error('Method not implemented.');
+  }
+
+    private appoitments: Appointment[]=[]
+
+    public async create({
+      provider_id,
+      date,
+    }: ICreateAppointmentDTO): Promise<Appointment> {
+      const appointment = new Appointment();
+
+      Object.assign(appointment, { id: uuid(), date, provider_id });
+
+      this.appoitments.push(appointment);
+      return appointment;
+    }
+
+    /**
+     * findByDate
+date: Date : Appointment | null    */
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
+      const findAppointment = this.appoitments.find((appointment) => appointment.date === date);
+      return findAppointment;
+    }
+}
+
+export default FakeAppointmentsRepository;
---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,22 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '252514',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  it('should not be able to create a two appointments on the same time', () => {
+    const data = {
+
+    };
+  });
+});
